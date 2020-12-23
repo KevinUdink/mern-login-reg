e30875c436a1d4e3ae58e9eaf533688a09018071
Kevin Udink <kudink@gmail.com>
2020-12-23 09:19:08 -0700

    Full Login and Reg project working From lecture on 12/22/20

---
+++ client/src/views/LogReg.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import SignIn from "../Components/SignIn";
+import SignUp from "../Components/SignUp";
+
+const LogReg = () => {
+  return (
+    <div className="container-flex">
+      <SignIn />
+      <SignUp />
+    </div>
+  );
+};
+
+export default LogReg;
